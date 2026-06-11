3cc7bf06cbb69cda11bff84b1271889304267eec
unknown <phipsipy@gmail.com>
2026-06-11 07:45:04 +0000

    clean: remove duplicate hash-password.js at root

--- backend/hash-password.js
+++
@@ -1,6 +0,0 @@
-import bcrypt from 'bcrypt';
-
-const password = 'Phipsipy2001@archive'; // ← à changer
-const salt = await bcrypt.genSalt(12);
-const hash = await bcrypt.hash(password, salt);
-console.log('ADMIN_PASSWORD_HASH=' + hash);
